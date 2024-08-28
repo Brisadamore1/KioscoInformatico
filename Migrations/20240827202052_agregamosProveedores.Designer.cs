2ed9d27ad6d561d2ffa44db696141663e28ee662
aleramirezsj <aleramirezsj@gmail.com>
2024-08-28 12:22:40 -0300

    Migración creada

--- Migrations/20240827202052_agregamosProveedores.Designer.cs
+++ Migrations/20240827202052_agregamosProveedores.Designer.cs
@@ -4,6 +4,7 @@ using KioscoInformatico.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KioscoInformatico.Migrations
 {
     [DbContext(typeof(KioscoContext))]
-    partial class KioscoContextModelSnapshot : ModelSnapshot
+    [Migration("20240827202052_agregamosProveedores")]
+    partial class agregamosProveedores
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
